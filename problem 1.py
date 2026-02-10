6db46796a0565cec8cab71ca3d2292e91a93d1b4
sweta kumari <swetakumari8541961415@gmail.com>
2026-02-10 16:13:38 +0530

    problem 1.py

---
+++ problem 1.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        left = 0
+        right = len(arr)-1
+        while left < right:
+            arr[left],arr[right] = arr[right],arr[left]
+            left += 1
+            right -= 1
