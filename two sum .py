75b38313f0188310d9e4a37e8554c7a9a4a91a34
sweta kumari <swetakumari8541961415@gmail.com>
2026-02-10 21:36:24 +0530

    twoSum .py

---
+++ two sum .py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums, target):
+        d = {}
+        for i in range(len(nums)):
+            if target - nums[i] in d:
+                return [d[target - nums[i]], i]
+            d[nums[i]] = i
