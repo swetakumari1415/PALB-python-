b68f1b0db36d1b30936be55787c63b6620158d6c
sweta kumari <swetakumari8541961415@gmail.com>
2026-02-12 11:44:49 +0530

    maxWater.py
    Implement maxWater function to calculate trapped rainwater.

---
+++ maxWater.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def maxWater(self, arr):
+        # code here
+        n = len(arr)
+        left = 0
+        right = n - 1
+        left_max = 0
+        right_max = 0
+        water = 0
+
+        while left <= right:
+            if arr[left] <= arr[right]:
+                if arr[left] >= left_max:
+                    left_max = arr[left]
+                else:
+                    water += left_max - arr[left]
+                left += 1
+            else:
+                if arr[right] >= right_max:
+                    right_max = arr[right]
+                else:
+                    water += right_max - arr[right]
+                right -= 1
+
+        return water
